0973cf013e5867d38352a2256a00b1e3e3e94467
Aswany-Santhosh <146819487+Aswany-Santhosh@users.noreply.github.com>
2024-03-11 15:20:30 +0530

    Add files via upload

---
+++ CharSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CharSort{
+	public static void main(String[] args){
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter the Strings:");
+	String str=scanner.nextLine();
+	
+	char[] chars=str.toCharArray();
+	
+	Arrays.sort(chars);
+	String sorted=new String(chars);
+	
+	System.out.println("\nSorted characters of the string:"+ sorted);
+	scanner.close();
+	}
+}
+	
+	
